a7afa67bd989d5cdaec103dc9351c071cfa11441
Hadi Raza <hadiraza.edu@gmail.com>
2026-03-18 15:52:26 +0500

    convert salt value type into integer

--- env.js
+++ env.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 export const PORT=process.env.PORT
 // export const mongoDB_URI= process.env.mongoDB_URI
 export const mongoDB_URI=process.env.mongoDB_URI
-export const SALT_ROUNDS=process.env.SALT_ROUNDS
+export const SALT_ROUNDS=Number(process.env.SALT_ROUNDS)
 export const JWT_EXPIRES_IN=process.env.JWT_EXPIRES_IN
 export const JWT_Secret=process.env.JWT_Secret
 export const GOOGLE_CLIENT_ID=process.env.GOOGLE_CLIENT_ID
